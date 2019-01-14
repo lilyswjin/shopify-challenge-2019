89cb0ba046b948e7f310001f5b82386ebd174494
Lily Jin <lily.sw.jin@gmail.com>
2019-01-14 15:34:25 -0500

    completed and styled header

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <h1 className="header__title">
+                Toronto Waste Lookup
+            </h1>
+        </header>
+    )
+}
